56d9e5ac9eaba2504ac64ff3ae2bad595319890c
phamtanphat <phatdroid94@gmail.com>
2022-02-23 19:07:08 +0700

    khai bao bien

--- src/Buoi1.java
+++ src/Buoi1.java
@@ -7,8 +7,21 @@ public class Buoi1 {
         // float , double ( kiểu số thực hay được số có phần thập phân)
         // boolean (kiểu dữ liệu chỉ 2 miền giá trị là đúng hoặc sai)
         // char (kiểu ký tự)
-
         // Nhóm 2 : Kiểu dữ liệu đối tượng (Object type)
 
+        // 2 : Khai báo biến
+//        char code = 'p';
+//        String name = "Phạm Tấn Phát";
+//        String phone = "0352408366";
+//        String address = "Lý Thường Kiệt Quận 10";
+//        int age = 27;
+
+        // Phương thức
+        // ctrl + p : kiểm tra kiểu dữ liệu truyền vào phương thức
+//        System.out.println("Code : " + code);
+//        System.out.println("Name : " + name);
+//        System.out.println("Phone : " + phone);
+//        System.out.println("Address : " + address);
+//        System.out.println("Age : " + age);
     }
 }
