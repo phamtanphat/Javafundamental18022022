f16c97ec0c78deb253caa404c513faf68e0f665c
phamtanphat <phatdroid94@gmail.com>
2022-02-23 18:32:00 +0700

    1 : kiểu dữ liệu

--- src/Buoi1.java
+++ src/Buoi1.java
@@ -1,5 +1,14 @@
 public class Buoi1 {
     public static void main(String[] args){
-        
+        // 1 : Kiểu dữ liệu
+        // Nhóm 1 : Kiểu dữ liệu nguyên thủy (Primitives type)
+        // String (chuỗi)
+        // int , long ( kiểu số nguyên)
+        // float , double ( kiểu số thực hay được số có phần thập phân)
+        // boolean (kiểu dữ liệu chỉ 2 miền giá trị là đúng hoặc sai)
+        // char (kiểu ký tự)
+
+        // Nhóm 2 : Kiểu dữ liệu đối tượng (Object type)
+
     }
 }
